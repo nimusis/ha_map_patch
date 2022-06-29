b0d363b98818d241e6ef59131442c43f012f15c2
nimusis <nimusis@gmail.com>
2022-06-29 19:18:45 +0900

    Update ha_map_patch.sh

--- ha_map_patch.sh
+++ ha_map_patch.sh
@@ -1,45 +1,68 @@
-#!/bin/bash
+!/bin/bash
+
+set -e
+
+RED_COLOR='\033[0;31m'
+GREEN_COLOR='\033[0;32m'
+GREEN_YELLOW='\033[1;33m'
+NO_COLOR='\033[0m'
+
+function info () { echo -e "${GREEN_COLOR}INFO: $1${NO_COLOR}";}
+function warn () { echo -e "${GREEN_YELLOW}WARN: $1${NO_COLOR}";}
+function error () { echo -e "${RED_COLOR}ERROR: $1${NO_COLOR}"; if [ "$2" != "false" ]; then exit 1;fi; }
+
+function checkRequirement () {
+        if [ -z "$(command -v "$1")" ]; then
+                error "'$1' is not installed"
+        fi
+}
+
+checkRequirement "wget"
 
 BASE_PATH=./
 
 case "${1:-''}" in
     'docker')
         BASE_PATH=`find /usr -name hass_frontend -type d`
-	echo -e "find hass_frontend directory path. ($BASE_PATH)\n"
-        ;;  
+        info "find hass_frontend directory. ('$BASE_PATH')"
+        ;;
 esac
 
 cd $BASE_PATH
 
-result=${PWD##*/}
-result=${result:-/}
+cur_dir=${PWD##*/}
 
-echo -e "current dir = $result\n"
 
-if [ $result != 'hass_frontend' ]
-then
-	echo "current directory is not 'hass_frontend'!!"
-	exit 1
+if [ $cur_dir != 'hass_frontend' ]; then
+        error "current directory is not 'hass_frontend'!! ('$cur_dir')"
 fi
 
-ES5_TARGET_FILES=`grep -nrl 'basemaps.cartocdn.com' ./frontend_es5/*.js`
+declare ES5_TARGET_FILES=($(grep -nrl 'basemaps.cartocdn.com' ./frontend_es5/*.js))
 
-for i in $ES5_TARGET_FILES; do
-        echo -e "patch file : $i"
-	cp $i ${i}.backup
-	sed -i 's/\"https:\/\/{s}.basemaps.cartocdn.com\/\".*maxZoom:20/"https:\/\/map.pstatic.net\/nrb\/styles\/"\.concat\(t\?"satellite":"basic","\/\{z\}\/\{x\}\/\{y\}\.png\?mt\=bg\.ol\.ts\.ar\.lko"\),\{minZoom:6,maxZoom:19,continuousWorld:\!0/g' $i
-	gzip -f -k $i
+for targetFile in "${ES5_TARGET_FILES[@]}"; do
+        info "patch file : '$targetFile'"
+        cp $targetFile ${targetFile}.backup
+        sed -i 's/\"https:\/\/{s}.basemaps.cartocdn.com\/\".*maxZoom:20/"https:\/\/map.pstatic.net\/nrb\/styles\/"\.concat\(t\?"satellite":"basic","\/\{z\}\/\{x\}\/\{y\}\.png\?mt\=bg\.ol\.ts\.ar\.lko"\),\{minZoom:6,maxZoom:19,continuousWorld:\!0/g' $targetFile
+        gzip -f -k $targetFile
 done
 
 
-LATEST_TARGET_FILES=`grep -nrl 'basemaps.cartocdn.com' ./frontend_latest/*.js`
+if [ ${#ES5_TARGET_FILES[@]} -eq 0 ]; then
+        warn "nothing to patch on frontend_es5/"
+fi
 
-for i in $LATEST_TARGET_FILES; do
-        echo -e "patch file : $i"
-	cp $i ${i}.backup
-	sed -i 's/`https:\/\/{s}.basemaps.cartocdn.com\/.*maxZoom:20/`https:\/\/map.pstatic.net\/nrb\/styles\/\$\{t\?"satellite":"basic"\}\/\{z\}\/\{x\}\/\{y\}.png\?mt\=bg\.ol\.ts\.ar\.lko\`,\{minZoom:6,maxZoom:19,continuousWorld:\!0/g' $i
-	gzip -f -k $i
+
+declare LATEST_TARGET_FILES=($(grep -nrl 'basemaps.cartocdn.com' ./frontend_latest/*.js))
+
+for targetFile in "${LATEST_TARGET_FILES[@]}"; do
+        info "patch file : '$targetFile'"
+        cp $targetFile ${targetFile}.backup
+        sed -i 's/`https:\/\/{s}.basemaps.cartocdn.com\/.*maxZoom:20/`https:\/\/map.pstatic.net\/nrb\/styles\/\$\{t\?"satellite":"basic"\}\/\{z\}\/\{x\}\/\{y\}.png\?mt\=bg\.ol\.ts\.ar\.lko\`,\{minZoom:6,maxZoom:19,continuousWorld:\!0/g' $targetFile
+        gzip -f -k $targetFile
 done
 
-echo "success!!"
+if [ ${#LATEST_TARGET_FILES[@]} -eq 0 ]; then
+        warn "nothing to patch on frontend_latest/"
+fi
 
+info "Success!!"
